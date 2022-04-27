871a595b1cea236f26c984351ba017a52e4813c9
Zachary Ferguson <zfergus@nyu.edu>
2022-04-27 14:51:22 -0400

    Cleaned up 2D HO tool

--- tools/weights/higher_order.py
+++ tools/weights/higher_order.py
@@ -1,125 +1,51 @@
 import numpy as np
 import igl
 import meshio
-import h5py
 import argparse
 import pathlib
 import scipy.sparse
+import itertools
+
+from utils import (
+    save_weights,
+    write_obj,
+    boundary_to_full,
+    attach_higher_order_nodes,
+    faces_to_edges
+)
 
 
-def save_weights(path, W):
-    h5f = h5py.File(path, 'w')
-
-    if scipy.sparse.issparse(W):
-        # Saving as sparse matrix
-        W_coo = W.tocoo()
-        g = h5f.create_group('weight_triplets')
-        g.create_dataset('values', data=W_coo.data)
-        g.create_dataset('rows', data=W_coo.row)
-        g.create_dataset('cols', data=W_coo.col)
-        g.attrs['shape'] = W_coo.shape
-    else:
-        h5f.create_dataset('weights', data=W)
-
-    h5f.close()
-
-
-def write_obj(filename, V, E=None, F=None):
-    with open(filename, 'w') as f:
-        for v in V:
-            f.write("v {:.16f} {:.16f} {:.16f}\n".format(*v))
-        if E is not None:
-            for e in E:
-                f.write("l {:d} {:d}\n".format(*(e + 1)))
-        if F is not None:
-            for face in F:
-                f.write("f {:d} {:d} {:d}\n".format(*(face + 1)))
+hat_phis = {
+    1: [lambda x: 1 - x, lambda x: x],
+    2: [
+        lambda x: 2 * (x - 0.5) * (x - 1),
+        lambda x: 2 * (x - 0) * (x - 0.5),
+        lambda x: -4 * (x - 0.5)**2 + 1
+    ],
+    3: [
+        lambda x: -9 / 2 * (x - 1 / 3) * (x - 2 / 3) * (x - 1),
+        lambda x: 9 / 2 * (x - 0) * (x - 1 / 3) * (x - 2 / 3),
+        lambda x: 27 / 2 * (x - 0) * (x - 2 / 3) * (x - 1),
+        lambda x: -27 / 2 * (x - 0) * (x - 1 / 3) * (x - 1),
+    ]
+}
 
 
-def hat_phi_1_0(x):
-    return 1 - x
-
-
-def hat_phi_1_1(x):
-    return x
-
-
-def hat_phi_2_0(x):
-    return 2 * (x - 0.5) * (x - 1)
-
-
-def hat_phi_2_1(x):
-    return 2 * (x - 0) * (x - 0.5)
-
-
-def hat_phi_2_2(x):
-    return -4 * (x - 0.5)**2 + 1
-
-
-def hat_phi_3_0(x):
-    return -9 / 2 * (x - 1 / 3) * (x - 2 / 3) * (x - 1)
-
-
-def hat_phi_3_1(x):
-    return 9 / 2 * (x - 0) * (x - 1 / 3) * (x - 2 / 3)
-
-
-def hat_phi_3_2(x):
-    return 27 / 2 * (x - 0) * (x - 2 / 3) * (x - 1)
-
-
-def hat_phi_3_3(x):
-    return -27 / 2 * (x - 0) * (x - 1 / 3) * (x - 1)
-
-
-def hat_phis(order):
-    if order == 1:
-        return [hat_phi_1_0, hat_phi_1_1]
-    elif order == 2:
-        return [hat_phi_2_0, hat_phi_2_1, hat_phi_2_2]
-    elif order == 3:
-        return [hat_phi_3_0, hat_phi_3_1, hat_phi_3_2, hat_phi_3_3]
-    else:
-        raise NotImplementedError()
-
-
-def get_highorder_nodes(V, F, order):
-    E = igl.edges(F)
-    V_new = V.tolist()
-    for e in E:
-        v1, v2 = V[e]
-        if order == 2:
-            V_new.append((v1 + v2) / 2)
-        elif order == 3:
-            V_new.append(v1 + (v2 - v1) / 3)
-            V_new.append(v1 + 2 * (v2 - v1) / 3)
-        else:
-            raise NotImplementedError(f"P{order} not implemented!")
-    for f in F:
-        v1, v2, v3 = V[f]
-        if order == 2:
-            break
-        elif order == 3:
-            V_new.append((v1 + v2 + v3) / 3)
-        else:
-            raise NotImplementedError(f"P{order} not implemented!")
-    return np.array(V_new)
-
-
-def get_phi_2d(num_nodes, n_vertex_nodes, E, E_boundary_to_E_full, order, div_per_edge):
+def get_phi_2d(num_nodes, num_vertices, E_boundary, E_boundary_to_E_full,
+               order, div_per_edge):
     assert(div_per_edge > 2)
     alpha = np.linspace(0, 1, div_per_edge)
     alpha = alpha[1:-1]
 
-    n_el = E.shape[0]
-    num_boundary_vertices = len(set(E.flatten()))
+    n_el = E_boundary.shape[0]
+    num_boundary_vertices = len(set(E_boundary.flatten()))
     num_coll_nodes = num_boundary_vertices + n_el * (div_per_edge - 2)
     n_edge_nodes = order - 1
 
     E_col = np.empty((n_el * (div_per_edge - 1), 2), dtype=int)
 
     V_edge_to_V_collision = {}
-    for e in E:
+    for e in E_boundary:
         for i in e:
             if i not in V_edge_to_V_collision:
                 V_edge_to_V_collision[i] = len(V_edge_to_V_collision)
@@ -133,13 +59,17 @@ def get_phi_2d(num_nodes, n_vertex_nodes, E, E_boundary_to_E_full, order, div_pe
     delta_vi = alpha.size
     start_ei = 0
     delta_ei = (div_per_edge - 1)
-    for ei, e in enumerate(E):
-        v_indices = np.append(
-            e, np.array(range(n_edge_nodes)) + n_vertex_nodes + E_boundary_to_E_full[ei] * n_edge_nodes)
+    for ei, e in enumerate(E_boundary):
+        vertex_nodes = e.tolist()
+        edge_nodes = (
+            num_vertices + E_boundary_to_E_full[ei] *
+            n_edge_nodes + np.arange(n_edge_nodes)
+        ).tolist()
+        nodes = vertex_nodes + edge_nodes
 
         for i in range(order + 1):
-            val = hat_phis(order)[i](alpha)
-            ind = v_indices[i]
+            val = hat_phis[order][i](alpha)
+            ind = nodes[i]
             phi[start_vi:(start_vi + delta_vi), ind] = val
 
         E_col[start_ei] = [V_edge_to_V_collision[e[0]], start_vi]
@@ -155,30 +85,23 @@ def get_phi_2d(num_nodes, n_vertex_nodes, E, E_boundary_to_E_full, order, div_pe
     return phi, E_col
 
 
-def find(e, E):
-    for i, ei in enumerate(E):
-        if (e == ei).all() or (e == ei[::-1]).all():
-            return i
-    raise Exception()
-
-
-def polyfem_ordering(num_vertices, F, order):
+def polyfem_ordering_2D(num_vertices, E, F, order):
     ordering = []
-    E = igl.edges(F)
+    F2E = faces_to_edges(F, E)
     processed_vertices = set()
     processed_edges = set()
-    num_nodes_per_edge = 1 if order == 2 else 2
+    num_nodes_per_edge = order - 1
     for fi, f in enumerate(F):
-        for v in f:
-            if v not in processed_vertices:
-                ordering.append(v)
-                processed_vertices.add(v)
+        for vi in f:
+            if vi not in processed_vertices:
+                ordering.append(vi)
+                processed_vertices.add(vi)
         for i in range(3):
-            e = find((f[i], f[(i + 1) % 3]), E)
-            if e not in processed_edges:
-                for j in range(num_nodes_per_edge):
-                    ordering.append(num_vertices + num_nodes_per_edge * e + j)
-                processed_edges.add(e)
+            ei = F2E[fi, i]
+            if ei not in processed_edges:
+                ordering.extend(num_vertices + num_nodes_per_edge * ei
+                                + np.arange(num_nodes_per_edge))
+                processed_edges.add(ei)
         if order == 3:
             ordering.append(num_vertices + E.shape[0] + fi)
     return ordering
@@ -199,20 +122,20 @@ def main():
     assert(mesh.cells[0].type == "triangle")
     F = mesh.cells[0].data
     V = mesh.points
-    n_vertex_nodes = V.shape[0]
+    num_vertices = V.shape[0]
 
     E_full = igl.edges(F)
     E_boundary = igl.boundary_facets(F)
-    boundary_to_full = [find(e, E_full) for e in E_boundary]
+    E_boundary_to_E_full = boundary_to_full(E_boundary, E_full)
 
     # insert higher order indices at end
-    V = get_highorder_nodes(V, F, args.order)
+    V = attach_higher_order_nodes(V, E_full, F, args.order)
 
     # get phi matrix
-    phi, E_col = get_phi_2d(V.shape[0], n_vertex_nodes, E_boundary,
-                            boundary_to_full, args.order, args.div_per_edge)
+    phi, E_col = get_phi_2d(V.shape[0], num_vertices, E_boundary,
+                            E_boundary_to_E_full, args.order, args.div_per_edge)
 
-    ordering = polyfem_ordering(n_vertex_nodes, F, args.order)
+    ordering = polyfem_ordering_2D(num_vertices, E_full, F, args.order)
     assert(len(ordering) == phi.shape[1])
     phi = phi[:, ordering]
     V = V[ordering]
@@ -224,7 +147,7 @@ def main():
     # radius = 0.5
     # for i in range(E_boundary.shape[0]):
     #     for j in range(args.order - 1):
-    #         vi = n_vertex_nodes + (args.order - 1) * boundary_to_full[i] + j
+    #         vi = num_vertices + (args.order - 1) * boundary_to_full[i] + j
     #         print(vi)
     #         point = V[vi]
     #         center_to_point = point - center
@@ -242,4 +165,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()  # input is the order
+    main()
