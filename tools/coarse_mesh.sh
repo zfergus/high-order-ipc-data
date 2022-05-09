f6450583529c786ac16c0ba7d8924f8bfa52f956
Zachary Ferguson <zfergus@nyu.edu>
2022-05-09 14:28:50 -0400

    Added gargoyle weights

--- tools/coarse_mesh.sh
+++ tools/coarse_mesh.sh
@@ -5,5 +5,5 @@ filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-tetwild -i "${1}" -o "${filename}.msh" -l 0.1 -e 0.003 --no-color
-# tetwild -i "${1}" -o "${filename}.msh" -l 0.1 -e 0.01 --no-color
+# tetwild -i "${1}" -o "${filename}.msh" -l 0.1 -e 0.003 --no-color
+tetwild -i "${1}" -o "${filename}.msh" -l 0.1 -e 0.01 --no-color
